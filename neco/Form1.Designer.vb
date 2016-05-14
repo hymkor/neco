eb6b63cf0c9b662f35389382f964fe0d13370ace
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-05-15 02:35:50 +0900

    first commit
    - text-encoding is current-codepage

---
+++ neco/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Font = New System.Drawing.Font("MS UI Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(3, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(578, 22)
+        Me.TextBox1.TabIndex = 0
+        '
+        'ListView1
+        '
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ListView1.Location = New System.Drawing.Point(3, 27)
+        Me.ListView1.MultiSelect = False
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(578, 232)
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.List
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 261)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = ".NET Peco"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ neco/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Private AllItem As New List(Of String)
+
+    Private Sub DoUpdate()
+        Me.ListView1.Clear()
+        Dim prefix As String = Me.TextBox1.Text.ToLower()
+        For Each p As String In AllItem
+            If p.ToLower().StartsWith(prefix) Then
+                Me.ListView1.Items.Add(p)
+            End If
+        Next
+        If Me.ListView1.Items.Count > 0 Then
+            Me.ListView1.Items(0).Selected = True
+            Me.ListView1.Select()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim require_update = False
+        While Console.In.Peek() >= 0
+            Dim line1 As String = Console.In.ReadLine()
+            Me.AllItem.Add(line1)
+            require_update = True
+        End While
+        If require_update Then
+            DoUpdate()
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        DoUpdate()
+    End Sub
+
+    Private Sub Done()
+        For Each p As ListViewItem In ListView1.SelectedItems
+            Console.WriteLine(p.Text)
+        Next
+        Me.Close()
+    End Sub
+
+    Private Sub ListView1_DoubleClick(sender As Object, e As EventArgs) Handles ListView1.DoubleClick
+        Done()
+    End Sub
+
+    Private Sub ListView1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ListView1.KeyPress
+        Dim index1 = If(Me.ListView1.SelectedItems.Count > 0, Me.ListView1.SelectedItems(0).Index, -1)
+        Select Case e.KeyChar
+            Case vbCr, vbLf
+                Done()
+            Case "n"c, "j"c, Chr(Asc("n"c) And &H1F)
+                Me.ListView1.Items((index1 + 1) Mod Me.ListView1.Items.Count).Selected = True
+            Case "p"c, "k"c, Chr(Asc("p"c) And &H1F)
+                Me.ListView1.Items((index1 + Me.ListView1.Items.Count - 1) Mod Me.ListView1.Items.Count).Selected = True
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Console.InputEncoding = System.Text.Encoding.UTF8
+    End Sub
+End Class
